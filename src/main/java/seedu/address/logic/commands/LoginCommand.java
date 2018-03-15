28536206d97ce40c5b9a222cd14fa3048aa7a27f
Qiu Haohao <qiuh0008@e.ntu.edu.sg>
2018-03-15 14:51:21 +0800

    Added Login Command

--- src/main/java/seedu/address/logic/commands/LoginCommand.java
+++ src/main/java/seedu/address/logic/commands/LoginCommand.java
@@ -1,4 +1,43 @@
 package seedu.address.logic.commands;
 
-public class LoginCommand {
+import seedu.address.model.Model;
+import seedu.address.model.person.NameContainsKeywordsPredicate;
+
+public class LoginCommand extends Command{
+    public static final String COMMAND_WORD = "login";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Login as student or librarian.\n"
+            + "Parameters: USERNAME PASSWORD\n"
+            + "Example: " + COMMAND_WORD + " MyUsername MyPassword";
+
+    public static final String MESSAGE_LOGGED_IN_AS_STUTENT = "You are logged in as student";
+    public static final String MESSAGE_LOGGED_IN_AS_LIBRARIAN = "You are logged in as librarian";
+    public static final String MESSAGE_NOT_LOGGED_IN = "Wrong username/password, try again";
+
+    public static final int PRIVILEGE_LEVEL = Model.PRIVILEGE_LEVEL_GUEST;
+
+    private final String username;
+    private final String password;
+
+
+    public LoginCommand(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public CommandResult execute() {
+        int newPrivilegeLevel = model.authenticate(username,password);
+        switch (newPrivilegeLevel){
+            case Model.PRIVILEGE_LEVEL_GUEST:
+                return new CommandResult(MESSAGE_NOT_LOGGED_IN);
+            case Model.PRIVILEGE_LEVEL_STUDENT:
+                return new CommandResult(MESSAGE_LOGGED_IN_AS_STUTENT);
+            case Model.PRIVILEGE_LEVEL_LIBRARIAN:
+                return new CommandResult(MESSAGE_LOGGED_IN_AS_LIBRARIAN);
+            default:
+                return new CommandResult(MESSAGE_NOT_LOGGED_IN);
+        }
+
+    }
 }
--- src/main/java/seedu/address/logic/parser/LoginCommandParser.java
+++ src/main/java/seedu/address/logic/parser/LoginCommandParser.java
@@ -1,4 +1,23 @@
 package seedu.address.logic.parser;
 
-public class LoginCommandParser {
+import seedu.address.logic.commands.LoginCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+
+public class LoginCommandParser implements Parser<LoginCommand>{
+
+    public LoginCommand parse(String args) throws ParseException {
+        String trimmedArgs = args.trim();
+        String[] nameKeywords = trimmedArgs.split("\\s+");
+        if (nameKeywords.length != 2) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, LoginCommand.MESSAGE_USAGE));
+        }
+
+        String username = nameKeywords[0];
+        String password = nameKeywords[1];
+
+        return new LoginCommand(username, password);
+    }
 }

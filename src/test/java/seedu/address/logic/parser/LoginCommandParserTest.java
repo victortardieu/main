8f52ca1be602af49317b5f291da2388bbe12ffbb
Qiu Haohao <qiuh0008@e.ntu.edu.sg>
2018-03-22 14:28:14 +0800

    Add LoginCommandParserTest

---
+++ src/test/java/seedu/address/logic/parser/LoginCommandParserTest.java
@@ -0,0 +1,27 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import org.junit.Test;
+import seedu.address.logic.commands.LoginCommand;
+
+public class LoginCommandParserTest {
+    private LoginCommandParser parser = new LoginCommandParser();
+
+    @Test
+    public void parse_emptyArg_throwsParseException() {
+        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, LoginCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_validArgs_returnsFindCommand() {
+        // no leading and trailing whitespaces
+        LoginCommand expectedFindCommand =
+                new LoginCommand("admin","admin");
+        assertParseSuccess(parser, "admin admin", expectedFindCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " \n admin \n \t admin  \t", expectedFindCommand);
+    }
+}

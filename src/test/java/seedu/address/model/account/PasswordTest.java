396264a0693425531218cebeb111c2d52e627f56
Qiu Haohao <qiuh0008@e.ntu.edu.sg>
2018-03-28 13:17:05 +0800

    Update part of the test

--- src/test/java/seedu/address/model/account/PasswordTest.java
+++ src/test/java/seedu/address/model/account/PasswordTest.java
@@ -1,5 +1,46 @@
+package seedu.address.model.account;
+
 import static org.junit.Assert.*;
+import org.junit.Test;
+import seedu.address.testutil.Assert;
 
 public class PasswordTest {
 
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        Assert.assertThrows(NullPointerException.class, () -> new Password(null));
+    }
+
+    @Test
+    public void isValidPassword() {
+        // null pointer
+        Assert.assertThrows(NullPointerException.class, () -> Password.isValidPassword(null));
+
+        //invalid
+        assertFalse(Password.isValidPassword("")); // empty string
+        assertFalse(Password.isValidPassword("123")); // too short
+        assertFalse(Password.isValidPassword("abc")); // too short
+        assertFalse(Password.isValidPassword("!!!")); // too short and non-word characters
+        assertFalse(Password.isValidPassword("!!!!!!")); // non-word characters
+        assertFalse(Password.isValidPassword("abcasj!")); // too short and non-word characters
+        assertFalse(Password.isValidPassword("abcde"));
+
+        //valid
+        assertTrue(Password.isValidPassword("abcde"));
+        assertTrue(Password.isValidPassword("banana"));
+        assertTrue(Password.isValidPassword("addressbook"));
+        assertTrue(Password.isValidPassword("abcde123"));
+        assertTrue(Password.isValidPassword("FHAIgasjd123987514"));
+        assertTrue(Password.isValidPassword("123123123123"));
+        assertTrue(Password.isValidPassword(""));
+
+    }
+
+    @Test
+    public void getPassword() {
+    }
+
+    @Test
+    public void equals() {
+    }
 }

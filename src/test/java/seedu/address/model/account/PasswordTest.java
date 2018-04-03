d9dc3d5cb265d31f2b6eee1f2f2b8522d44d39ff
Qiu Haohao <qiuh0008@e.ntu.edu.sg>
2018-04-03 16:16:36 +0800

    Update tests

--- src/test/java/seedu/address/model/account/PasswordTest.java
+++ src/test/java/seedu/address/model/account/PasswordTest.java
@@ -23,7 +23,7 @@ public class PasswordTest {
         assertFalse(Password.isValidPassword("!!!")); // too short and non-word characters
         assertFalse(Password.isValidPassword("!!!!!!")); // non-word characters
         assertFalse(Password.isValidPassword("abcasj!")); // too short and non-word characters
-        assertFalse(Password.isValidPassword("abcde"));
+        assertFalse(Password.isValidPassword(""));
 
         //valid
         assertTrue(Password.isValidPassword("abcde"));
@@ -32,15 +32,36 @@ public class PasswordTest {
         assertTrue(Password.isValidPassword("abcde123"));
         assertTrue(Password.isValidPassword("FHAIgasjd123987514"));
         assertTrue(Password.isValidPassword("123123123123"));
-        assertTrue(Password.isValidPassword(""));
+
 
     }
 
     @Test
     public void getPassword() {
+        String passwordString = "password";
+        Password p = new Password(passwordString);
+        assertEquals(passwordString, p.getPassword());
     }
 
     @Test
     public void equals() {
+        Password p1 = new Password("password1");
+        Password p1copy = new Password("password1");
+        Password p2 = new Password("password2");
+
+        //equal with itself
+        assertTrue(p1.equals(p1));
+
+        //equal with an other object with same state
+        assertTrue(p1.equals(p1copy));
+
+        //not equal with null
+        assertFalse(p1.equals(null));
+
+        //not equal with other type
+        assertFalse(p1.equals(1));
+
+        //not equal with same type with different state
+        assertFalse(p1.equals(p2));
     }
 }

64060617d3734f20b01f5a9d6459d4c7db682975
Qiu Haohao <qiuh0008@e.ntu.edu.sg>
2018-04-04 23:31:10 +0800

    Update test

--- src/test/java/seedu/address/model/account/CredentialTest.java
+++ src/test/java/seedu/address/model/account/CredentialTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+//import javax.jws.soap.SOAPBinding;
+
 import org.junit.Test;
 
 import seedu.address.testutil.Assert;
@@ -55,5 +57,21 @@ public class CredentialTest {
 
         //not equal with same type with different state
         assertFalse(c1.equals(c2));
+    }
+
+    @Test
+    public void usernameEquals() {
+        String u1 = "username1";
+        String u2 = "username2";
+        String p1 = "password1";
+        Credential u1p1 = new Credential(u1, p1);
+        Credential u1p2 = new Credential(u1, p1);
+        Credential u2p1 = new Credential(u2, p1);
+        Username username1 = u1p1.getUsername();
+        Username username2 = u2p1.getUsername();
+
+        assertTrue(u1p1.usernameEquals(username1));
+        assertTrue(u1p2.usernameEquals(username1));
+        assertFalse(u1p1.usernameEquals(username2));
     }
 }
